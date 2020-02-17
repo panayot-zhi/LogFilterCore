b6b831c5a9152be038196c88da9f82a730bad7a5
panayot-zhi <panayot_zhi@abv.bg>
2020-02-17 15:44:55 +0200

    Update program, visual.

--- LogFilterCore/Program.cs
+++ LogFilterCore/Program.cs
@@ -1,15 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LogFilterCore.Utility;
+using System;
+using System.Threading;
 
 namespace LogFilterCore
 {
     internal class Program
     {
+        private const string Hello = @"
+
+╦  ┌─┐┌─┐╔═╗┬┬ ┌┬┐┌─┐┬─┐╔═╗┌─┐┬─┐┌─┐
+║  │ ││ ┬╠╣ ││  │ ├┤ ├┬┘║  │ │├┬┘├┤
+╩═╝└─┘└─┘╚  ┴┴─┘┴ └─┘┴└─╚═╝└─┘┴└─└─┘
+
+";
+
         private static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments!");
+                Console.WriteLine("Exiting...");
+                Thread.Sleep(1000);
+                return;
+            }
+
+            foreach (var line in Hello.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
+            {
+                Console.WriteLine(line);
+                Thread.Sleep(500);
+            }
+
+            void ReportProgress(string message, int? percent)
+            {
+                if (percent.HasValue)
+                {
+                    if (string.IsNullOrEmpty(message))
+                    {
+                        Console.Write($"\rProgress: {percent}%");
+                    }
+                    else if (message.HasPlaceholder())
+                    {
+                        Console.Write($"\r{string.Format(message, percent)}");
+                    }
+                    else
+                    {
+                        Console.Write($"\r{message}: " + percent + "%");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+            var runner = new ConfigurationRunner(ReportProgress);
+
+            foreach (var arg in args)
+            {
+                if (!FileHelper.IsFile(arg))
+                {
+                    Console.WriteLine("Does not exist: " + arg);
+                    Thread.Sleep(500);
+                    continue;
+                }
+
+                try
+                {
+                    runner.Run(arg);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Console.WriteLine("Exiting...");
+            Thread.Sleep(1000);
         }
     }
 }
