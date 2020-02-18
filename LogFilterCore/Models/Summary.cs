f8af941e44d3bf7fd06d2331d02616f7dd3bee9b
panayot-zhi <panayot_zhi@abv.bg>
2020-02-18 14:47:35 +0200

    Trim references.

--- LogFilterCore/Models/Summary.cs
+++ LogFilterCore/Models/Summary.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LogFilterCore.Models.Filters;
 using LogFilterCore.Utility;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
