f786730f272815ff927300862d7b6c399f389dab
panayot-zhi <panayot_zhi@abv.bg>
2020-02-19 13:44:11 +0200

    Elaborate.

--- LogFilterCore/Utility/FilterType.cs
+++ LogFilterCore/Utility/FilterType.cs
@@ -5,21 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LogFilterCore.Utility
-{
+{    
     public enum FilterType
     {
         /// <summary>
-        /// If the filter matched a line, include the entry in the main result set.
+        /// If the filter matched a line, include the entry in the main result set and continue with another entry.
         /// </summary>
         Include,
 
         /// <summary>
-        /// If the filter matched a line, exclude the entry from the main result set.
+        /// If the filter matched a line, exclude the entry from the main result set and continue with another entry.
         /// </summary>
         Exclude,
 
         /// <summary>
-        /// If the filter matched a line, write the entry to a different result set.
+        /// If the filter matched a line, write the entry to a different result set and continue with another entry.
         /// </summary>
         WriteToFile
     }
