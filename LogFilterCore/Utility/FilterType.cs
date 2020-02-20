077c87ea58280c9750d9794fc60888eb88c02e98
panayot-zhi <panayot_zhi@abv.bg>
2020-02-20 14:20:48 +0200

    Rearrange for better understanding.

--- LogFilterCore/Utility/FilterType.cs
+++ LogFilterCore/Utility/FilterType.cs
@@ -9,11 +9,6 @@ namespace LogFilterCore.Utility
     public enum FilterType
     {
         /// <summary>
-        /// If the filter matched a line, include the entry in the main result set and continue with another entry.
-        /// </summary>
-        Include,
-
-        /// <summary>
         /// If the filter matched a line, exclude the entry from the main result set and continue with another entry.
         /// </summary>
         Exclude,
@@ -21,6 +16,16 @@ namespace LogFilterCore.Utility
         /// <summary>
         /// If the filter matched a line, write the entry to a different result set and continue with another entry.
         /// </summary>
-        WriteToFile
+        WriteToFile,
+
+        /// <summary>
+        /// If the filter matched a line, write the entry to a both the specific filter result set and the output filtered result set.
+        /// </summary>
+        IncludeAndWriteToFile,
+
+        /// <summary>
+        /// If the filter matched a line, include the entry in the main result set and continue with another entry.
+        /// </summary>
+        Include,
     }
 }
