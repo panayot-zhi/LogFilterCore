b800b65fdfeb7242ecfc30060bd073444fbfd26f
panayot-zhi <panayot_zhi@abv.bg>
2020-02-20 14:20:33 +0200

    This does not belong here.

--- LogFilterCore/Utility/Extensions.cs
+++ LogFilterCore/Utility/Extensions.cs
@@ -14,36 +14,5 @@ namespace LogFilterCore.Utility
         {
             return Regex.IsMatch(s, "{\\d+}");
         }
-
-        /*private static void MarkAndAddEntry(ICollection<LogEntry> list, LogEntry currentEntry)
-        {
-            currentEntry.InResultSet = true;
-            list.Add(currentEntry);
-        }
-
-        public static void AddLogEntry(this List<LogEntry> me, LogEntry currentEntry, LogEntry[] sourceList, int context, int? index = null)
-        {
-            if (context == 0)
-            {
-                MarkAndAddEntry(me, currentEntry);
-                return;
-            }
-
-            if (!index.HasValue)
-            {
-                index = Array.IndexOf(sourceList, currentEntry);
-            }
-
-            var current = index.Value - context;
-            while (current <= index.Value + context)
-            {
-                if (current >= 0 && current < sourceList.Length && !sourceList[current].InResultSet)
-                {
-                    MarkAndAddEntry(me, sourceList[current]);
-                }
-
-                current++;
-            }
-        }*/
     }
 }
