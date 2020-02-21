3e58db1e1ee317e5e74b909b924593c4d03613b1
panayot-zhi <panayot_zhi@abv.bg>
2020-02-21 10:49:51 +0200

    Yet another run.

--- LogFilterCore/ConfigurationRunner.cs
+++ LogFilterCore/ConfigurationRunner.cs
@@ -42,7 +42,7 @@ namespace LogFilterCore
             Run();
         }
 
-        protected void Run()
+        protected virtual void Run()
         {
             var cfg = Current;
             if (!string.IsNullOrWhiteSpace(cfg.InputFolder))
@@ -69,8 +69,7 @@ namespace LogFilterCore
 
             cfg.Parser = InstantiateParser(cfg.ParserName);
 
-            var parser = cfg.Parser;
-            var filters = cfg.Filters;
+            var parser = cfg.Parser;            
             var runSummary = RunSummary;
             BeginRunSummary(cfg.Parser.DateTimeFormat);
                     
@@ -81,65 +80,260 @@ namespace LogFilterCore
             {
                 ReportProgress("No log files found in input folder or none passed pre-filtering.", 100);
                 return;
-            }
-
-            void ProgressCallback(int percent)
-            {
-                ReportProgress("Progress: {0}", percent);
-            }                      
+            }                
             
             // NOTE: files are ordered here by LastWriteTime
             // reverse it to preserve the order in the output files            
             foreach (var fileInfo in inputFiles.Reverse())
             {
-                var filePath = fileInfo.FullName;
-                var currentSummary = parser.BeginSummary();
-                ReportProgress($"Reading file '{filePath}'...");
-
-                var logLines = FileProcessor.ReadLogLines(filePath, ProgressCallback, out var linesRead, parser.Expression);
-
-                if (!string.IsNullOrEmpty(cfg.Reparse))
+                try
                 {
-                    // if we're reparsing we need to replace the original file name (thhat's with a prefix)
-                    // with a one without a prefix, and prefix it accordingly during this parser run
-                    filePath = FileProcessor.ExtractFileName(filePath, cfg.Reparse);
+                    Run(fileInfo);
+                }
+                catch (FileProcessorException fpex)
+                {
+                    ReportProgress(fpex.Message);
+                }
+                catch (ParserException pex)
+                {
+                    ReportProgress(pex.Message);
+
+                    if (parser.NonStandardLines.Any())
+                    {
+                        // write non-standard entries to tunSummary
+                        runSummary.NonStandardEntries += parser.NonStandardLines.Count;
+
+                        var filePath = fileInfo.FullName;
+                        var outputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, $"[{cfg.ParserName}-FAILED]-");
+                        if (FileProcessor.WriteFile(outputPath, parser.NonStandardLines, cfg.OverwriteFiles))
+                        {
+                            runSummary.FilesWritten++;
+                        }                                                                
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ReportProgress(ex.Message);
+                }
+            }
+
+            EndRunSummary();
+        }
+
+        protected virtual void Run(FileInfo fileInfo)
+        {
+            var cfg = Current;            
+            var parser = cfg.Parser;
+            var filters = cfg.Filters;
+            var runSummary = RunSummary;            
+
+            var filePath = fileInfo.FullName;
+            var currentSummary = parser.BeginSummary();
+            ReportProgress($"Reading file '{filePath}'...");
+
+            void ProgressCallback(int percent)
+            {
+                ReportProgress("Progress: {0}", percent);
+            }
+
+            var logLines = FileProcessor.ReadLogLines(filePath, ProgressCallback, out var linesRead, parser.Expression);
+
+            if (!string.IsNullOrEmpty(cfg.Reparse))
+            {
+                // if we're reparsing we need to replace the original file name (thhat's with a prefix)
+                // with a one without a prefix, and prefix it accordingly during this parser run
+                filePath = FileProcessor.ExtractFileName(filePath, cfg.Reparse);
+            }
+
+            runSummary.FilesRead++;
+            currentSummary.FilesRead++;
+            runSummary.LinesRead += (ulong)linesRead;
+            currentSummary.LinesRead = (ulong)linesRead;
+            runSummary.LogsRead += (ulong)logLines.Length;
+            currentSummary.LogsRead = (ulong)logLines.Length;
+
+            ReportProgress($"Lines: {linesRead}, Logs: {logLines.Length}, Constructing entries...");
+
+            var logEntries = parser.ToLogEntry(logLines).ToArray();
+            runSummary.NonStandardEntries += parser.NonStandardLines.Count;
+            currentSummary.NonStandardEntries = parser.NonStandardLines.Count;
+
+            if (parser.NonStandardLines.Any())
+            {
+                // write failed log entries and continue with the parsing run (if it didn't throw an exception, threshold has not been exceeded)
+                var outputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, $"[{cfg.ParserName}-FAILED]-");
+                if (FileProcessor.WriteFile(outputPath, parser.NonStandardLines, cfg.OverwriteFiles))
+                {
+                    runSummary.FilesWritten++;
+                    currentSummary.FilesWritten++;
+                }
+            }
+
+            runSummary.EntriesConstructed += (ulong)logEntries.Length;
+            currentSummary.EntriesConstructed = (ulong)logEntries.Length;
+
+            ReportProgress($"Logs: {logLines.Length}, Constructed: {logEntries.Length}, Filtering file...");
+
+            var filteredEntries = parser.FilterLogEntries(logEntries, ProgressCallback);
+
+            runSummary.FilteredEntries += (ulong)filteredEntries.Length;
+            currentSummary.FilteredEntries = (ulong)filteredEntries.Length;
+
+            ReportProgress($"Entries: {logEntries.Length}, Filtered: {filteredEntries.Length}, Writing files...");
+
+            if (filteredEntries.Any())
+            {
+                Split(filePath, filteredEntries, currentSummary);
+
+                // write filtered file                        
+                var filteredLines = parser.ToLines(filteredEntries).ToArray();
+
+                var filteredEntriesOutputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, "filtered");
+                if (FileProcessor.WriteFile(filteredEntriesOutputPath, filteredLines, cfg.OverwriteFiles))
+                {
+                    runSummary.FilesWritten++;
+                    currentSummary.FilesWritten++;
+
+                    runSummary.LinesWritten += (ulong)filteredLines.Length;
+                    currentSummary.LinesWritten = (ulong)filteredLines.Length;
                 }
 
-                runSummary.FilesRead++;
-                currentSummary.FilesRead++;
-                runSummary.LinesRead += (ulong)linesRead;
-                currentSummary.LinesRead = (ulong)linesRead;
-                runSummary.LogsRead += (ulong)logLines.Length;
-                currentSummary.LogsRead = (ulong)logLines.Length;
+                ReportProgress($"ALL: {filteredEntries.Length}");
+            }
+            else
+            {
+                // TODO: At level WARN!
+                ReportProgress("No filtered entries resulted after run.");
+            }
 
-                ReportProgress($"Lines: {linesRead}, Logs: {logLines.Length}, Constructing entries...");
-
-                var logEntries = parser.ToLogEntry(logLines).ToArray();
-                runSummary.NonStandardEntries += parser.NonStandardLines.Count;
-                currentSummary.NonStandardEntries = parser.NonStandardLines.Count;
-
-                if (parser.NonStandardLines.Any())
+            // write entries accumulated in the filters
+            var dedicatedFilterLogs = filters.Where(f => f.Type == FilterType.WriteToFile || f.Type == FilterType.IncludeAndWriteToFile);
+            foreach (var dedicatedFilter in dedicatedFilterLogs)
+            {
+                // do not write empty files
+                if (!dedicatedFilter.Entries.Any())
                 {
-                    // write failed log entries and continue with the parsing run (if it didn't throw an exception, threshold has not been exceeded)
-                    var outputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, $"[{cfg.ParserName}-FAILED]-");
-                    if (FileProcessor.WriteFile(outputPath, parser.NonStandardLines, cfg.OverwriteFiles))
+                    continue;
+                }
+
+                // write one file per each custom filter that requires output
+                var dedicatedFilterOutputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, dedicatedFilter.Name);
+                //dedicatedFilter.FileName = dedicatedFilterOutputPath; // TODO: Figure out what's the idea here?
+                if (FileProcessor.WriteFile(dedicatedFilterOutputPath, parser.ToLines(dedicatedFilter.Entries), cfg.OverwriteFiles))
+                {
+                    runSummary.FilesWritten++;
+                    currentSummary.FilesWritten++;
+                }
+            }
+
+            if (cfg.CopyOriginal)
+            {
+                ReportProgress("Writing original file...");
+
+                // write the original file to output folder
+                var originalOutputFilePath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, "original");
+                if (FileProcessor.WriteFile(originalOutputFilePath, logLines, cfg.OverwriteFiles))
+                {
+                    runSummary.FilesWritten++;
+                    currentSummary.FilesWritten++;
+                }
+            }
+
+            // end current summary
+            parser.EndSummary();
+
+            // write summary, current summary
+            var currentSummaryOutputPath = FileProcessor.GetCurrentSummaryFilePath(filePath, cfg.InputFolder, cfg.OutputFolder);
+            if (FileProcessor.WriteFile(currentSummaryOutputPath, currentSummary.ToJson(), cfg.OverwriteFiles))
+            {
+                FileProcessor.SetReadonly(currentSummaryOutputPath);
+                runSummary.FilesWritten++;
+            }
+
+            AggregateRunSummaryCounters(currentSummary.Filters);
+
+            ReportProgress("Done.");
+        }
+
+        protected void Split(string filePath, LogEntry[] filteredEntries, Summary currentSummary)
+        {
+            var cfg = Current;
+            var parser = cfg.Parser;
+            var runSummary = RunSummary;
+
+            if (cfg.SplitByThreads != null)
+            {
+                // whether or not we should write file for each thread
+                var splitByAllThreads = cfg.SplitByThreads.Length == 0;
+
+                var groupsByKey = filteredEntries.GroupBy(entry => entry.Thread);
+                foreach (var groupedEntries in groupsByKey)
+                {
+                    // if we're not splitting by all threads, other threads should be skipped
+                    if (!splitByAllThreads && !cfg.SplitByThreads.Contains(groupedEntries.Key))
+                    {
+                        continue;
+                    }
+
+                    var currentThreadOutputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, $"[THREAD#{groupedEntries.Key}]-");
+                    if (FileProcessor.WriteFile(currentThreadOutputPath, parser.ToLines(groupedEntries), cfg.OverwriteFiles))
                     {
                         runSummary.FilesWritten++;
                         currentSummary.FilesWritten++;
                     }
+
+                    ReportProgress($"THREAD#{groupedEntries.Key}: {groupedEntries.Count()}");
                 }
+            }
 
-                runSummary.EntriesConstructed += (ulong)logEntries.Length;
-                currentSummary.EntriesConstructed = (ulong)logEntries.Length;
+            if (cfg.SplitByIdentities != null)
+            {
+                // whether or not we should write file for each identity
+                var splitByAllIdentities = cfg.SplitByIdentities.Length == 0;
+                                    
+                var groupsByKey = filteredEntries.GroupBy(entry => entry.Identity);
+                foreach (var groupedEntries in groupsByKey)
+                {
+                    // if we're not splitting by all users, other users should be skipped
+                    if (!splitByAllIdentities && !cfg.SplitByIdentities.Contains(groupedEntries.Key))
+                    {
+                        continue;
+                    }
 
-                ReportProgress($"Logs: {logLines.Length}, Constructed: {logEntries.Length}, Filtering file...");
+                    var currentIdentityOutputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, $"[IDENTITY#{groupedEntries.Key}]-");
+                    if (FileProcessor.WriteFile(currentIdentityOutputPath, parser.ToLines(groupedEntries), cfg.OverwriteFiles))
+                    {
+                        runSummary.FilesWritten++;
+                        currentSummary.FilesWritten++;
+                    }
 
-                var filteredEntries = parser.FilterLogEntries(logEntries, ProgressCallback);
+                    ReportProgress($"IDENTITY#{groupedEntries.Key}: {groupedEntries.Count()}");
+                }
+            }
 
-                runSummary.FilteredEntries += (ulong)filteredEntries.Length;
-                currentSummary.FilteredEntries = (ulong)filteredEntries.Length;
+            if (cfg.SplitByLogLevels != null)
+            {
+                // whether or not we should write file for each log level
+                var splitByAllLogLevels = cfg.SplitByLogLevels.Length == 0;
 
-                ReportProgress($"Entries: {logEntries.Length}, Filtered: {filteredEntries.Length}, Writing files...");
+                var groupsByKey = filteredEntries.GroupBy(entry => entry.Level);
+                foreach (var groupedEntries in groupsByKey)
+                {
+                    // if we're not splitting by all log levels, other levels should be skipped
+                    if (!splitByAllLogLevels && !cfg.SplitByLogLevels.Contains(groupedEntries.Key))
+                    {
+                        continue;
+                    }
+
+                    var currentLogLevelOutputPath = FileProcessor.GetOutputFilePath(filePath, cfg.InputFolder, cfg.OutputFolder, $"[LEVEL#{groupedEntries.Key}]-");
+                    if (FileProcessor.WriteFile(currentLogLevelOutputPath, parser.ToLines(groupedEntries), cfg.OverwriteFiles))
+                    {
+                        runSummary.FilesWritten++;
+                        currentSummary.FilesWritten++;
+                    }
+
+                    ReportProgress($"LEVEL#{groupedEntries.Key}: {groupedEntries.Count()}");
+                }
             }
         }
 
@@ -230,7 +424,7 @@ namespace LogFilterCore
 
             if (!(Activator.CreateInstance(type) is ParserBase parser))
             {
-                throw new ParserException($"Could not create a parser instance with the type of '{type}'.");
+                throw new ConfigurationException($"Could not create a parser instance with the type of '{type}'.");
             }
 
             return parser;
@@ -245,6 +439,15 @@ namespace LogFilterCore
             var filtersCopy = Current.Filters.Clone();
             filtersCopy.ForEach((x) => { x.Count = 0; });
             RunSummary.Filters = filtersCopy.ToArray();            
+        }
+
+        private void AggregateRunSummaryCounters(IEnumerable<Filter> filters)
+        {
+            foreach (var filter in filters)
+            {
+                // TODO: This relies on filter naming to match the one in the summary, which is not guaranteed
+                RunSummary.Filters.Single(x => x.Name == filter.Name).Count += filter.Count;
+            }
         }
 
         private void EndRunSummary()
